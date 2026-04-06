fb9e9d3f61c418393f467fdb7e12767c18f2b229
Faiz Ridho <faizridhonew@gmail.com>
2026-04-06 16:17:25 +0700

    fix globals css import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 import GoogleAnalytics from "../lib/components/GoogleAnalytics";
 const geistSans = Geist({
   variable: "--font-geist-sans",
