8f4dcde73d9974f9b445f4e22b6cb67a7dad764c
Faiz Ridho <faizridhonew@gmail.com>
2026-04-06 14:24:09 +0700

    update lagi

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig & {
+  eslint?: {
+    ignoreDuringBuilds?: boolean;
+  };
+} = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
